7040cb6282f60e645e6e6c921fc910817f10f404
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-11-02 12:35:28 +0530

    Adding Pseudo Public Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "../node_modules/@coreui/coreui/dist/css/coreui.min.css";
-import { ToastsContainer, ToastsStore, ToastsContainerPosition } from "react-toasts";
-import './App.scss';
-import SignUp from './pages/signup-page';
-import LoginPage from './pages/login-page';
-import ForgotPasswordPage from './pages/forgot-password-page';
+import {
+  ToastsContainer,
+  ToastsStore,
+  ToastsContainerPosition,
+} from "react-toasts";
+import "./App.scss";
+import SignUp from "./pages/signup-page";
+import LoginPage from "./pages/login-page";
+import ForgotPasswordPage from "./pages/forgot-password-page";
+import ProfilePreview from "./pages/profile-preview-page";
 import DefaultLayout from "./containers/DefaultLayout/DefaultLayout";
-import {store,persistor} from './redux/store';
-import {PersistGate} from "redux-persist/integration/react";
-import {Provider} from 'react-redux';
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
 function App() {
+  const username = localStorage.getItem("username");
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
@@ -36,6 +47,11 @@ function App() {
               />
 
               <Route path="/" component={DefaultLayout} />
+              <Route
+                exact
+                path="/profile-preview/"
+                component={ProfilePreview}
+              />
               <Route path="*" render={() => <Redirect to="/" />} />
             </Switch>
           </div>
